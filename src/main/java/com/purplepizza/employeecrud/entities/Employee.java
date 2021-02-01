cb2351497615ea5ac07d748523ffc96ba53bf475
Ana Luisa Dias <analudias@live.com>
2021-02-01 19:06:26 -0300

    Employee entity

---
+++ src/main/java/com/purplepizza/employeecrud/entities/Employee.java
@@ -0,0 +1,113 @@
+package com.purplepizza.employeecrud.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Employee implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String address;
+	private Double salary;
+	private String seniorityLevel;
+	private String role;
+	private LocalDateTime entryDate;
+	
+	public Employee() {
+	}
+
+	public Employee(Long id, String name, String address, Double salary, String seniorityLevel, String role,
+			LocalDateTime entryDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.salary = salary;
+		this.seniorityLevel = seniorityLevel;
+		this.role = role;
+		this.entryDate = entryDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public String getSeniorityLevel() {
+		return seniorityLevel;
+	}
+
+	public void setSeniorityLevel(String seniorityLevel) {
+		this.seniorityLevel = seniorityLevel;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public LocalDateTime getEntryDate() {
+		return entryDate;
+	}
+
+	public void setEntryDate(LocalDateTime entryDate) {
+		this.entryDate = entryDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
