4ea86b239726f50ed6e7fb3fd9848d314aeb2190
Ana Luisa Dias <analudias@live.com>
2021-02-01 19:37:40 -0300

    Maven dependencies and h2 configuration

--- src/main/java/com/purplepizza/employeecrud/entities/Employee.java
+++ src/main/java/com/purplepizza/employeecrud/entities/Employee.java
@@ -1,18 +1,31 @@
 package com.purplepizza.employeecrud.entities;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.security.Timestamp;
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_employee")
 public class Employee implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String address;
 	private Double salary;
 	private String seniorityLevel;
 	private String role;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private LocalDateTime entryDate;
 	
 	public Employee() {
